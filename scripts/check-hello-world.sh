9d623e7f2e1203fc7b6a2c900d3843a5f97def17
Aaryn Olsson <aaryno@gmail.com>
2020-12-08 00:22:06 -0700

    update

--- scripts/check-hello-world.sh
+++ scripts/check-hello-world.sh
@@ -12,13 +12,13 @@ if [ -z "$url" ]; then
 fi
 echo 1
 is_this_repo=$(echo $url | grep github.com/ua-gist-open-source)
-if [ -n $is_this_repo ]; then
+if [ -n "$is_this_repo" ]; then
     echo URL should be for your newly created github repo 
     exit 1
 fi
 echo 1
 is_user_repo=$(echo $url | grep github.com/$USER)
-if [ -z $is_user_repo ]; then
+if [ -z "$is_user_repo" ]; then
     echo URL should probably have your USERNAME it: $url does not have \'$USER\' in it
     exit 1
 fi
@@ -27,7 +27,7 @@ status=$(curl -s -I -L -o /dev/null -w "%{http_code}" $url)
 echo 1
 ok=$(echo $status | grep ^2)
 echo 1
-if [ -z $ok ]; then
+if [ -z "$ok" ]; then
     echo URL $url not found or is not public: status: $status
     exit 1
 fi
